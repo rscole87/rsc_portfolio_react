7972565d5119626bb63348fcfa86b407f378dfb6
Rodrick Coleman <rscole87@gmail.com>
2021-08-05 13:50:39 -0400

    add project modal component and fix modal display bug

--- src/components/ProjectAreaComponent.js
+++ src/components/ProjectAreaComponent.js
@@ -1,46 +1,15 @@
 import React, { Component } from "react";
-import { Modal, ModalHeader, ModalFooter, ModalBody } from "reactstrap";
 import PROJECTS from "../shared/projectList";
-
-function RenderProject(props){
-  return (
-    <Modal id="project-modal" isOpen={props.isModalOpen} toggle={props.toggleModal}>
-      <ModalHeader toggle={props.toggleModal}>
-        <h4 className="project-name" id="active-project-name">
-          {props.project.name}
-        </h4>
-      </ModalHeader>
-      <ModalBody>
-        <div className="modal-column-container">
-          <div className="modal-img-column">
-            <video className="project-vid" id="active-project-video" src={props.project.video} project-vid controls loop autoplay webkit-playsinline playsinline>
-              Your browser does not support the video tag.
-            </video>
-          </div>
-
-          <div className="modal-description-column">
-            <p className="project-description" id="active-project-description" project-description>
-              {props.project.description}
-            </p>
-
-            <div className="modal-bttn-container">
-              <a className="code-bttn" id="active-project-demo" href={props.project.demo} target="_blank">
-                Live Code
-              </a>
-              <a className="code-bttn" id="active-project-github" href={props.project.github} target="_blank">
-                Source Code
-              </a>
-            </div>
-          </div>
-        </div>
-      </ModalBody>
-    </Modal>
-  );
-};
+import ProjectModal from "./ProjectModalComponent";
 
 
 function ProjectThumb(props) {
-  return <div className="project-thumb" data-aos="fade-up" data-aos-delay="150" data-aos-duration="1000" style={{ backgroundImage: `url(${props.project.image})` }} onClick={() => (<RenderProject project={props.project} isOpen={true} toggleModal={props.toggleModal}/>)} />;
+  return ( 
+    <div className="project-thumb" data-aos="fade-up" data-aos-delay="150" data-aos-duration="1000" 
+      style={{ backgroundImage: `url(${props.project.image})` }} 
+      onClick={() => props.setActiveProject(props.project)}  
+      />
+  )
 }
 class ProjectArea extends Component {
   constructor(props) {
@@ -48,11 +17,20 @@ class ProjectArea extends Component {
     this.state = {
       projects: PROJECTS,
       isModalOpen: false,
+      activeProject: PROJECTS[0],
     };
+
+    this.toggleModal = this.toggleModal.bind(this);
+    this.setActiveProject = this.setActiveProject.bind(this);
   }
 
   toggleModal() {
     this.setState({ isModalOpen: !this.state.isModalOpen });
+  }
+
+  setActiveProject(project) {
+    this.setState({isModalOpen: true, activeProject: project})
+    console.log(this.state.activeProject)
   }
 
   render() {
@@ -68,7 +46,7 @@ class ProjectArea extends Component {
 
         <div id="projects-grid">
           {displayedProjects.map((project) => (
-            <ProjectThumb project={project} isOpen={this.state.isModalOpen} toggleModal={this.toggleModal} />
+            <ProjectThumb project={project} setActiveProject={this.setActiveProject}/>
           ))}
         </div>
 
@@ -80,6 +58,8 @@ class ProjectArea extends Component {
             <p className="body-copy">Send me message or feel free to connect with me via my social media links below.</p>
           </div>
         </div>
+          
+        <ProjectModal project={this.state.activeProject} isModalOpen={this.state.isModalOpen} toggleModal={this.toggleModal} />
       </section>
     );
   }
