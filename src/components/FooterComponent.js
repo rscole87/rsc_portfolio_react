b6847820a14dd371a7ceddcb0eb3ce28a51094bd
Rodrick Coleman <rscole87@gmail.com>
2021-08-08 15:50:49 -0400

    add logo text, columns, and hover transitions to footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logoType from "../images/logo-text-light-blue.png";
 
 function Footer() {
   return (
@@ -6,12 +7,16 @@ function Footer() {
       <footer>
         <div className="container">
           <div id="footer-container" className="row">
-            <div className="col-12 col-sm-6">
-              <h4 className="footer-heading">Rodrick S. Coleman - Web Developer</h4>
-              <p className="body-copy">Let's create something great together!</p>
+            <div className="col-12 col-sm-6 col-md-4 text-center">
+              <div>
+                <img className="branding" src={logoType} alt="Rodrick Coleman | Web Developer" />
+              </div>
+              <div className="pt-3">
+                <p className="body-copy">Let's create something great together!</p>
+              </div>
             </div>
 
-            <div id="footer-menu-div" className="col-12 col-sm-6 navbar">
+            <div id="footer-menu-div" className="col-12 col-sm-6 col-md-4 d-flex justify-content-center navbar">
               <ul id="footer-menu" className="navbar-nav d-flex">
                 <li>
                   <a className="my-nav-link" href="#about-section">
@@ -29,6 +34,10 @@ function Footer() {
                   </a>
                 </li>
               </ul>
+            </div>
+
+            <div className="col-12 col-sm-6 col-md-4">
+
             </div>
           </div>
         </div>
